8d8c79f5e1f56d05027f98abaf072049f332cace
maxidiaz9 <141974881+maxidiaz9@users.noreply.github.com>
2023-10-18 21:06:09 -0300

    Add files via upload

---
+++ Insert tabla Calificacion.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: select * from calificación
+LIMIT 0, 1000
+
+-- Date: 2023-08-24 21:23
+*/
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (1,1,3,23);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (2,0,2,11);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (3,1,4,10);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (4,0,5,9);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (5,1,6,8);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (6,0,3,22);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (7,1,8,5);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (8,0,4,6);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (9,1,5,6);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (10,0,6,3);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (11,1,1,28);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (12,1,21,29);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (13,0,22,3);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (14,1,25,4);
+INSERT INTO `` (`ID_Calificación`,`Puntuación`,`Id_Usuario`,`ID_PeliculaSerie`) VALUES (15,0,29,1);
---
+++ Insert tabla Usuarios.sql
@@ -0,0 +1,36 @@
+/*
+-- Query: select* from usuarios
+LIMIT 0, 1000
+
+-- Date: 2023-08-24 21:24
+*/
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (1,'Bernadine','bartharg0@mozilla.com','Premium','mastercard',34594304);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (2,'Carling','cesparza1@wordpress.com','Estandar','VISA',32349504);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (3,'Dode','dbeckham2@ebay.co.uk','Estandar','VISA',30104704);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (4,'Cesaro','cjakoviljevic3@bluehost.com','Premium','mastercard',27859904);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (5,'Jillayne','jodempsey4@sakura.ne.jp','Básico','VISA',25615104);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (6,'Vonni','vrobrose5@boston.com','Estandar','mastercard',23370304);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (7,'Reena','rcookman6@sakura.ne.jp','Premium','mastercard',21125504);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (8,'Bobette','bborrell7@macromedia.com','Estandar','VISA',18880704);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (9,'Raymond','rfinnemore8@youtube.com','Estandar','VISA',16635904);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (10,'Torre','tchoffin9@irs.gov','Premium','VISA',14391104);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (11,'Nicolis','nellershawa@weather.com','Básico','mastercard',12146304);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (12,'Mannie','mgladdenb@sun.com','Premium','mastercard',9901504);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (13,'Claudine','cjahnckec@about.me','Básico','mastercard',7656704);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (14,'Corena','ctomasellid@hud.gov','Premium','VISA',5411904);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (15,'Evaleen','edjekice@wunderground.com','Estandar','VISA',3167104);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (16,'Woodie','waldersf@wikispaces.com','Básico','VISA',922304);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (17,'Ardelle','awethg@g.co','Estandar','VISA',13933949);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (18,'Eydie','ehaberghamh@furl.net','Básico','VISA',11233493);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (19,'Tracy','ttrickeyi@a8.net','Estandar','mastercard',8533037);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (20,'Clair','cbankesj@admin.ch','Premium','VISA',5832581);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (21,'Sashenka','sholylandk@java.com','Básico','mastercard',3132125);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (22,'Parnell','pcurgenuerl@skyrock.com','Básico','mastercard',12403403);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (23,'Willard','wtourotm@t-online.de','Estandar','mastercard',5767444);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (24,'Darryl','dlawrencesonn@intel.com','Básico','VISA',4569833);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (25,'Mollee','mteasdalemarkieo@dyndns.org','Premium','VISA',3372222);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (26,'Rheba','rmenearp@businesswire.com','Estandar','VISA',2174611);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (27,'Ann','aollierq@homestead.com','Estandar','mastercard',977000);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (28,'Camala','cstroudr@blogs.com','Estandar','mastercard',45554444);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (29,'Donal','ddowdalls@nih.gov','Estandar','VISA',44333222);
+INSERT INTO `` (`Id_Usuario`,`NOMBRE`,`Correo_electronico`,`Tipo_de_plan`,`Forma_de_pago`,`DNI`) VALUES (30,'Ardys','aaerst@earthlink.net','Básico','VISA',43112000);
